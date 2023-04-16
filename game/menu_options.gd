25b4bcfe53dd67ff1c5819398c839016b7478fd3
CombatMonkey11 <levanego1@gmail.com>
2023-04-15 20:40:19 -0400

    menu button update

--- game/menu_options.gd
+++ game/menu_options.gd
@@ -1,8 +1,9 @@
 extends RichTextLabel
 signal change_scene
 
-var x = Color.RED
-
+var rng = RandomNumberGenerator.new()
+var clr = Color.RED
+var val = rng.randi_range(0,5);
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,7 +14,21 @@ func _process(delta):
 	pass
 	
 func _on_mouse_entered():
-	add_theme_color_override("default_color", x)
+		add_theme_color_override("default_color", clr)
 	
 func _on_mouse_exited():
+	match val:
+		0:
+			clr = Color.RED
+		1:
+			clr = Color.AQUA
+		2:
+			clr = Color.BLUE
+		3: 
+			clr = Color.CORNFLOWER_BLUE
+		4: 
+			clr = Color.GREEN
+		5: 
+			clr = Color.GOLD
+	val = rng.randi_range(0,5);
 	add_theme_color_override("default_color", Color.WHITE)
