8850ee0c4565e699b100d21128b3aeda9c40d881
CombatMonkey11 <levanego1@gmail.com>
2023-04-15 20:55:49 -0400

    menu button handlers established

--- game/main.gd
+++ game/main.gd
@@ -9,7 +9,11 @@ func _ready():
 	game = load("res://game.tscn")
 	var instance = main_menu.instantiate()
 	add_child(instance)
-	$MainMenu/MarginContainer/HBoxContainer/VBoxContainer/MenuItems/NewGame.gui_input.connect(_on_gui_input)
+	$MainMenu/MarginContainer/HBoxContainer/VBoxContainer/MenuItems/NewGame.gui_input.connect(ng_handler)
+	$MainMenu/MarginContainer/HBoxContainer/VBoxContainer/MenuItems/Exit.gui_input.connect(exit_handler)
+	$MainMenu/MarginContainer/HBoxContainer/VBoxContainer/MenuItems/Credits.gui_input.connect(credits_handler)
+	$MainMenu/MarginContainer/HBoxContainer/VBoxContainer/MenuItems/Settings.gui_input.connect(settings_handler)
+	$MainMenu/MarginContainer/HBoxContainer/VBoxContainer/MenuItems/Continue.gui_input.connect(cg_handler)
 	pass # Replace with function body.
 
 
@@ -22,7 +26,19 @@ func _switch_scene():
 	var instance = game.instantiate()
 	add_child(instance)
 	
-func _on_gui_input(event):
+func ng_handler(event):
 	if event is InputEventMouseButton:
 		if event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT:
 			_switch_scene()
+
+func settings_handler(event):
+	pass
+
+func cg_handler(event):
+	pass
+	
+func exit_handler(event):
+	pass
+
+func credits_handler(event):
+	pass
