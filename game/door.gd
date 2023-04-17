6172123b3f95c982f7178d8fb409830df1f42453
Dimitrios Haralampopoulos <dimi.haralampopoulos@gmail.com>
2023-04-17 17:47:55 -0400

    Added doors

---
+++ game/door.gd
@@ -0,0 +1,24 @@
+extends Area2D
+
+@export var target_scene: PackedScene
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _input(event):
+	if event.is_action_pressed("Interact"):
+		if !target_scene:
+			print("no scene in this door")
+			return
+		if get_overlapping_bodies().size() > 0:
+			next_room()
+
+func next_room():
+	var ERR = get_tree().change_scene_to_packed(target_scene)
+	if ERR != OK:
+		print("Something failed in the door scene") 
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
